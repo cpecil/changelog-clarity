ceb0ffb3f18b0951d24338592eb4304b18e47bc1
Chaitanya Pandey <readopedia.in@gmail.com>
2026-01-20 12:08:08 +0530

    Update App.tsx
    updated app.tsx with legal pages routing API nodes

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,10 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
+import { TermsPage } from "@/components/m3/TermsPage";
+import { PrivacyPage } from "@/components/m3/PrivacyPage";
+import { CookiePage } from "@/components/m3/CookiePage";
+import { LicensePage } from "@/components/m3/LicensePage";
 
 const queryClient = new QueryClient();
 
@@ -16,6 +20,13 @@ const App = () => (
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Index />} />
+          
+          {/* Legal Routes */}
+          <Route path="/legal/terms" element={<TermsPage />} />
+          <Route path="/legal/privacy" element={<PrivacyPage />} />
+          <Route path="/legal/cookies" element={<CookiePage />} />
+          <Route path="/legal/license" element={<LicensePage />} />
+          
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
